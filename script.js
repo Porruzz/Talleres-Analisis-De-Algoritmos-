6f83278b05169f5d43005573e62c9a2f07d68d9c
Santiago Parra Acuña <164297730+Porruzz@users.noreply.github.com>
2025-02-24 21:19:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-document.getElementById("download-btn").addEventListener("click", function() {
-    alert("Tu descarga comenzará en breve.");
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".container").style.opacity = "0";
+    setTimeout(() => {
+        document.querySelector(".container").style.transition = "opacity 1s";
+        document.querySelector(".container").style.opacity = "1";
+    }, 300);
 });
